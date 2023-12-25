2f7193e513457c26c2c4dded4c57ad62db2fc0ff
Jessica <jessijob@rambler.ru>
2023-12-25 14:16:52 +0800

    switch

--- js/script_services_switch.js
+++ js/script_services_switch.js
@@ -1,11 +1,46 @@
-function switchPoints() {
-    const departureSelect = document.getElementById("departurePoint");
-    const arrivalSelect = document.getElementById("arrivalPoint");
+// Функция для переключения пунктов отправления и прибытия в модальном окне 1
+function switchPoints1() {
+    const departureSelect = document.getElementById("departurePoint1");
+    const arrivalSelect = document.getElementById("arrivalPoint1");
     const tempDepartureValue = departureSelect.value;
     const tempArrivalValue = arrivalSelect.value;
 
     departureSelect.value = tempArrivalValue;
     arrivalSelect.value = tempDepartureValue;
 }
+
+// Функция для переключения пунктов отправления и прибытия в модальном окне 2
+function switchPoints2() {
+    const departureSelect = document.getElementById("departurePoint2");
+    const arrivalSelect = document.getElementById("arrivalPoint2");
+    const tempDepartureValue = departureSelect.value;
+    const tempArrivalValue = arrivalSelect.value;
+
+    departureSelect.value = tempArrivalValue;
+    arrivalSelect.value = tempDepartureValue;
+}
+
+// Функция для переключения пунктов отправления и прибытия в модальном окне 3
+function switchPoints3() {
+    const departureSelect = document.getElementById("departurePoint3");
+    const arrivalSelect = document.getElementById("arrivalPoint3");
+    const tempDepartureValue = departureSelect.value;
+    const tempArrivalValue = arrivalSelect.value;
+
+    departureSelect.value = tempArrivalValue;
+    arrivalSelect.value = tempDepartureValue;
+}
+
+// Функция для переключения пунктов отправления и прибытия в модальном окне 4
+function switchPoints4() {
+    const departureSelect = document.getElementById("departurePoint4");
+    const arrivalSelect = document.getElementById("arrivalPoint4");
+    const tempDepartureValue = departureSelect.value;
+    const tempArrivalValue = arrivalSelect.value;
+
+    departureSelect.value = tempArrivalValue;
+    arrivalSelect.value = tempDepartureValue;
+}
+
 
 
